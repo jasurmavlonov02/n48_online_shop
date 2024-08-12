629b55dbe51f64e0df38a62a0247e0b114174b9a
jasurmavlonov02 <jasurmavlonov24@gmail.com>
2024-08-12 17:22:02 +0500

    register page done!

--- online_shop/forms.py
+++ online_shop/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from online_shop.models import Comment, Order, Product
 
 
-#
 # class CommentForm(forms.Form):
 #     name = forms.CharField(max_length=100)
 #     email = forms.EmailField()
@@ -51,3 +50,36 @@ class LoginForm(forms.Form):
     #     if not User.objects.filter(username=username).exists():
     #         raise forms.ValidationError(f'That user {username} not found')
     #     return username
+
+
+class RegisterForm(forms.ModelForm):
+    confirm_password = forms.CharField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def clean_username(self):
+        username = self.data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'This {username} is already exists')
+        return username
+
+    def clean_password(self):
+        password = self.data.get('password')
+        confirm_password = self.data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError('Passwords do not match')
+        return self.cleaned_data['password']
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        user.is_active = True
+        user.is_superuser = True
+        user.is_staff = True
+
+        if commit:
+            user.save()
+
+        return user
